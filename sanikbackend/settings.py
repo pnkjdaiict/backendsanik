0fca055361e50f0f0a0cbec13fa5ce4c764a0fca
pnkjdaiict <pnkjdaiict@gmail.com>
2024-11-18 14:41:31 +0530

    Update settings.py

--- sanikbackend/settings.py
+++ sanikbackend/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'sanikbackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopgg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'royaldefenceAcademy',
         'USER': 'admin',
         'PASSWORD':'admin#123',
