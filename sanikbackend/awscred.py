e0a249ba261e7a81b1f2a0bf2f89c3d51518e7ed
pnkjdaiict <pnkjdaiict@gmail.com>
2024-11-29 09:08:36 +0530

    Delete sanikbackend/awscred.py

--- sanikbackend/awscred.py
+++
@@ -1,8 +0,0 @@
-# import boto3
-
-# class AWS_CREDENTIAL:
-#     AWS_ACCESS_KEY=  'AKIAUMYCIT53LF2XR4F4',
-#     AWS_SECRET_ACCESS_KEY='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',
-#     BUCKET_NAME= 'rdabucket'
-
- 
