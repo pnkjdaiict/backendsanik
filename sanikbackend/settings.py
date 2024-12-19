ba4d9e353fd047fc2e68773f19f93854de3da215
unknown <pnkjdaiict@gmail.com>
2024-12-19 12:21:04 +0530

    test

--- sanikbackend/settings.py
+++ sanikbackend/settings.py
@@ -137,15 +137,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+ 
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+STATIC_URL = 'static/' 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
